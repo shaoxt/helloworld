91cb7ca031148ae17e1061e74c8f811a6d15c556
BuildTools <unconfigured@null.spigotmc.org>
2018-03-05 14:32:52 -0800

    Use stdin

--- push.sh
+++ push.sh
@@ -1,5 +1,5 @@
 # Just for demo, we need to use DockerHubAPI instead
 docker logout
-docker login --username=shaoxt --password=$1 https://index.docker.io/v1/
+docker login --username=shaoxt --password-stdin https://index.docker.io/v1/
 docker tag helloworld shaoxt/helloworld
 docker push shaoxt/helloworld:latest
