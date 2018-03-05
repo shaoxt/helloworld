3cad6435bb630cc267b08e54879e0d86afcf8236
BuildTools <unconfigured@null.spigotmc.org>
2018-03-05 14:30:00 -0800

    Fix push

--- push.sh
+++ push.sh
@@ -1,1 +1,6 @@
-docker push shaoxt/helloworld:latest
+# Just for demo, we need to use DockerHubAPI instead
+export DOCKER_ID_USER="shaoxt"
+docker logout
+docker login --username=$DOCKER_ID_USER --password=$1 https://index.docker.io/v1/
+docker tag helloworld $DOCKER_ID_USER/helloworld
+docker push $DOCKER_ID_USER/helloworld:latest
