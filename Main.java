42fbcaf22d0f2f33eee10b29069df38d4344d316
BuildTools <unconfigured@null.spigotmc.org>
2018-03-05 11:23:06 -0800

    Simplify

--- Main.java
+++ Main.java
@@ -6,24 +6,22 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 
-public class Main {
+public class Main implements HttpHandler {
     public static void main(String[] args) throws Exception {
         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
-        server.createContext("/helloworld", new HelloworldHandler());
+        server.createContext("/helloworld", new Main());
         server.setExecutor(null); // creates a default executor
         server.start();
 
         System.out.println("Started!");
     }
 
-    static class HelloworldHandler implements HttpHandler {
-        @Override
-        public void handle(HttpExchange t) throws IOException {
-            String response = "Hello World!";
-            t.sendResponseHeaders(200, response.length());
-            OutputStream os = t.getResponseBody();
-            os.write(response.getBytes());
-            os.close();
-        }
+    @Override
+    public void handle(HttpExchange t) throws IOException {
+        String response = "Hello World!";
+        t.sendResponseHeaders(200, response.length());
+        OutputStream os = t.getResponseBody();
+        os.write(response.getBytes());
+        os.close();
     }
 }
