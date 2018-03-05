ca45e424747a5ebab86c6e14324621ca50d5e54b
BuildTools <unconfigured@null.spigotmc.org>
2018-03-05 14:54:14 -0800

    Use file, try to make a workaround for docker login issue

--- push.sh
+++ push.sh
@@ -1,5 +1,5 @@
 #. Just for demo, we need to use DockerHubAPI instead
 docker logout
-docker login --username=shaoxt --password=$1 docker.io
+cat $1|docker login --username=shaoxt --password-stdin docker.io
 #docker tag helloworld shaoxt/helloworld:latest
 docker push shaoxt/helloworld:latest
