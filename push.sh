a35b3e973230e6ab3969bb9f7f48eda0e404401e
BuildTools <unconfigured@null.spigotmc.org>
2018-03-05 14:38:17 -0800

    Delete tag

--- push.sh
+++ push.sh
@@ -1,5 +1,5 @@
-# Just for demo, we need to use DockerHubAPI instead
+#. Just for demo, we need to use DockerHubAPI instead
 docker logout
-docker login --username=shaoxt --password-stdin https://index.docker.io/v1/
-docker tag helloworld shaoxt/helloworld
+docker login --username=shaoxt --password=$1 docker.io
+#docker tag helloworld shaoxt/helloworld:latest
 docker push shaoxt/helloworld:latest
