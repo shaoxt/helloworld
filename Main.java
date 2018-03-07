a19e51a81fe0e2e173f890437a8046cad9bd7228
Sheldon <shaoxt@gmail.com>
2018-03-06 21:41:00 -0800

    6

--- Main.java
+++ Main.java
@@ -19,7 +19,7 @@ public class Main implements HttpHandler {
 
     @Override
     public void handle(HttpExchange t) throws IOException {
-        String response = "Hello World! 5";
+        String response = "Hello World! 6";
         t.sendResponseHeaders(200, response.length());
         OutputStream os = t.getResponseBody();
         os.write(response.getBytes());
